7200cbfca825ec151fe64b5c4c07bde620e75742
rahul@3003 <rahulkhandke71@gmail.com>
2023-05-10 18:33:41 +0530

    live backend server change

--- client/script.js
+++ client/script.js
@@ -87,7 +87,8 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    // const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codex-rahul.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
